56289f8a7ee426b7ba64d532487b402430504c69
justin <games.j@live.nl>
2021-01-05 16:03:03 +0100

    Added media activity with videoview
    added video view and media activity

--- HololiveApp/app/src/main/java/com/example/hololiveapp/MediaActivity.java
+++ HololiveApp/app/src/main/java/com/example/hololiveapp/MediaActivity.java
@@ -1,6 +1,9 @@
 package com.example.hololiveapp;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,15 @@ public class MediaActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mediaplayer);
+
+        VideoView vidView = (VideoView) findViewById(R.id.video_view);
+        Uri uri = Uri.parse("rtsp://r7—sn-4g57kue6.googlevideo.com/Ck0LENy73wIaRAmk3cJBg-iaXhMYDSANFC0k8JRWMOCoAUIJbXYtZ29vZ2xlSARSBXdhdGNoYKK205Ti2LaNVooBC2lZYlRtN201YTlRDA==/9EFB79E36D6A4191F3BF60D01A0AE429B5C9AA32.B774C6D2C0948C3A49A088F9F5CCCFBE34864B29/yt6/1/video.3gp");
+        vidView.setVideoURI(uri);
+
+        MediaController medcontrol = new MediaController(this);
+        vidView.setMediaController(medcontrol);
+
+
+
     }
 }
