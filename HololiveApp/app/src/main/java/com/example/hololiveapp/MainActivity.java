02448359c4b486428c9545225dd94a8c94bc0829
justin <games.j@live.nl>
2021-01-05 14:56:37 +0100

    startup project android
    added basic android activities

---
+++ HololiveApp/app/src/main/java/com/example/hololiveapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.hololiveapp;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void PlayMedia(View view){
+
+        Intent mediaIntent = new Intent(MainActivity.this, MediaActivity.class);
+        startActivity(mediaIntent);
+    }
+
+}
