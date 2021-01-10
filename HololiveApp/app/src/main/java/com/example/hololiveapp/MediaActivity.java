3b67ba15e1a33d891e6b6e25e228918fe50b6334
justin <games.j@live.nl>
2021-01-10 15:32:05 +0100

    Fixed mediaplayer bugs and errors
    mediaplayer now stops and pauses when needed, and doesnt request the livestream or video over and over when pressing the fastforward, backwards  or pause button

--- HololiveApp/app/src/main/java/com/example/hololiveapp/MediaActivity.java
+++ HololiveApp/app/src/main/java/com/example/hololiveapp/MediaActivity.java
@@ -1,6 +1,7 @@
 package com.example.hololiveapp;
 
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,16 @@ public class MediaActivity extends AppCompatActivity {
     SimpleExoPlayer player;
     PlayerView playerView;
 
-
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mediaplayer);
+        configureMediaPlayer();
+        player.play();
+    }
+
+    public void configureMediaPlayer(){
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         player = new SimpleExoPlayer.Builder(this).build();
         playerView = findViewById(R.id.exoplayview);
         playerView.setPlayer(player);
@@ -28,6 +33,21 @@ public class MediaActivity extends AppCompatActivity {
         MediaItem mediaItem = MediaItem.fromUri("http://f24hls-i.akamaihd.net/hls/live/221147/F24_EN_HI_HLS/master_2000.m3u8");
         player.setMediaItem(mediaItem);
         player.prepare();
-        player.play();
     }
+
+    @Override
+    public void onBackPressed()
+    {
+        player.stop();
+        player.release();
+        playerView.onPause();
+        super.onBackPressed();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.stop();
+        playerView.onPause();
+    }
+
 }
