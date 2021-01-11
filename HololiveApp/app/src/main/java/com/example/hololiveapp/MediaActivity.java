55211069ae7353bdbb00770f6483debb76150d41
justin <games.j@live.nl>
2021-01-11 12:48:12 +0100

    updated .gitignore
    updated .gitignore

--- HololiveApp/app/src/main/java/com/example/hololiveapp/MediaActivity.java
+++ HololiveApp/app/src/main/java/com/example/hololiveapp/MediaActivity.java
@@ -27,9 +27,6 @@ public class MediaActivity extends AppCompatActivity {
         player = new SimpleExoPlayer.Builder(this).build();
         playerView = findViewById(R.id.exoplayview);
         playerView.setPlayer(player);
-        //video
-        //MediaItem mediaItem = MediaItem.fromUri("https://videocdn.bodybuilding.com/video/mp4/62000/62792m.mp4");
-        //livestream
         MediaItem mediaItem = MediaItem.fromUri("http://f24hls-i.akamaihd.net/hls/live/221147/F24_EN_HI_HLS/master_2000.m3u8");
         player.setMediaItem(mediaItem);
         player.prepare();
