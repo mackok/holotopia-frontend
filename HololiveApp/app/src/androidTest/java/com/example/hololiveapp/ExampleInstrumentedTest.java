62fad84cf60a7ac254513cce52f4f2627f29d34a
justin <games.j@live.nl>
2021-01-11 12:57:48 +0100

    updated gitignore yet agian
    updated gitignore yet agian

--- HololiveApp/app/src/androidTest/java/com/example/hololiveapp/ExampleInstrumentedTest.java
+++ HololiveApp/app/src/androidTest/java/com/example/hololiveapp/ExampleInstrumentedTest.java
@@ -21,5 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.hololiveapp", appContext.getPackageName());
+        //
     }
 }
