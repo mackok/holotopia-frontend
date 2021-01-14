018098a0d079fde3ea859bf39728002174562380
justin <games.j@live.nl>
2021-01-14 16:06:49 +0100

    Added javadocs to the app activities
    Added javadocs to the app activities

--- HololiveApp/app/src/main/java/com/example/hololiveapp/MoriBioActivity.java
+++ HololiveApp/app/src/main/java/com/example/hololiveapp/MoriBioActivity.java
@@ -3,9 +3,17 @@ package com.example.hololiveapp;
 import android.content.pm.ActivityInfo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * Class that contains the layout and operations of the activity_mori_bio.xml
+ */
 public class MoriBioActivity extends AppCompatActivity {
 
+    /**
+     * Creates and/or sets activity parameters,functions and objects needed to display and
+     * set the layout of the activity.
+     * @param savedInstanceState Bundle object that contains the previous state of this activity
+     *                           so that it can revert to a previous state when needed.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- HololiveApp/app/src/main/java/com/example/hololiveapp/VtuberLibraryActivity.java
+++ HololiveApp/app/src/main/java/com/example/hololiveapp/VtuberLibraryActivity.java
@@ -6,8 +6,17 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Class that contains the layout and operations of the activity_vtuber_library.xml
+ */
 public class VtuberLibraryActivity extends AppCompatActivity {
 
+    /**
+     * Creates and/or sets activity parameters,functions and objects needed to display and
+     * set the layout of the activity.
+     * @param savedInstanceState Bundle object that contains the previous state of this activity
+     *                           so that it can revert to a previous state when needed.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,25 +25,49 @@ public class VtuberLibraryActivity extends AppCompatActivity {
     }
 
 
+    /**
+     *  Used to navigate to the GuraBioActivity class
+     * @param view This class represents the basic building block for user interface components.
+     *             A View occupies a rectangular area on the screen and is responsible for drawing and event handling.
+     */
     public void goToGuraBio(View view){
-
         startActivity(new Intent(this,GuraBioActivity.class));
     }
 
+    /**
+     *  Used to navigate to the MoriBioActivity class
+     * @param view This class represents the basic building block for user interface components.
+     *             A View occupies a rectangular area on the screen and is responsible for drawing and event handling.
+     */
     public void goToMoriBio(View view){
-
         startActivity(new Intent(this,MoriBioActivity.class));
     }
-    public void goToNinoBio(View view){
 
+    /**
+     *  Used to navigate to the NinoBioActivity class
+     * @param view This class represents the basic building block for user interface components.
+     *             A View occupies a rectangular area on the screen and is responsible for drawing and event handling.
+     */
+    public void goToNinoBio(View view){
         startActivity(new Intent(this,NinoBioActivity.class));
     }
-    public void goToTakaBio(View view){
 
+    /**
+     *  Used to navigate to the TakaBioActivity class
+     * @param view This class represents the basic building block for user interface components.
+     *             A View occupies a rectangular area on the screen and is responsible for drawing and event handling.
+     */
+    public void goToTakaBio(View view){
         startActivity(new Intent(this,TakaBioActivity.class));
     }
-    public void goToWatsonBio(View view){
 
+    /**
+     *  Used to navigate to the WatsonBioActivity class
+     * @param view This class represents the basic building block for user interface components.
+     *             A View occupies a rectangular area on the screen and is responsible for drawing and event handling.
+     */
+    public void goToWatsonBio(View view){
         startActivity(new Intent(this,WatsonBioActivity.class));
     }
+
 }
